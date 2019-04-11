11d4189e0a583d94b9228ea0f02da61781cac1df
nikhil sulegaon <nikhilsulegaon@gmail.com>
2019-04-10 22:49:11 -0600

    Refactoring code. Not relying on Global variables.

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -7,7 +7,15 @@ from multiprocessing import cpu_count
 from tqdm import tqdm
 
 
-def resize_to_std_resolution(image_name):
+class ResizeImage(object):
+    def __init__(self, path_to_dataset):
+        self.path_to_dataset = path_to_dataset
+
+    def __call__(self, image):
+        resize_to_std_resolution(image, self.path_to_dataset)
+
+
+def resize_to_std_resolution(image_name, path_to_dataset):
     os.makedirs(os.path.join(path_to_dataset, 'high'), exist_ok=True)
     os.makedirs(os.path.join(path_to_dataset, 'low'), exist_ok=True)
     image = cv2.imread(os.path.join(path_to_dataset, 'data', image_name))
@@ -30,12 +38,12 @@ if __name__ == '__main__':
     if ('LOCAL' not in os.environ) and len(sys.argv) != 2:
         print('Usage: python prepare_dataset.py <path_to_dataset>')
     else:
-        path_to_dataset = sys.argv[1] if len(sys.argv) == 2 else 'dataset'
-        images = fetch_image_names(path_to_dataset)
+        path = sys.argv[1] if len(sys.argv) == 2 else 'dataset'
+        images = fetch_image_names(path)
 
         pool = Pool(cpu_count())
 
-        for _ in tqdm(pool.imap(resize_to_std_resolution, images), total=len(images)):
+        for _ in tqdm(pool.imap(ResizeImage(path), images), total=len(images)):
             pass
         pool.close()
         pool.join()
